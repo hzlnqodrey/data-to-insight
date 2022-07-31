c1c02192fbb66197f8aed0397cab95407287e8be
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-31 09:16:36 +0700

    calculate income

--- 1 - Exploring ​and ​Preparing ​your ​Data with BigQuery/high-quality-sql-in-bigquery.sql
+++ 1 - Exploring ​and ​Preparing ​your ​Data with BigQuery/high-quality-sql-in-bigquery.sql
@@ -1,6 +1,14 @@
-# standardSQL
+# standardSQL - Revenue
 SELECT
-    FORMAT("%'d", total_revenue) AS total_revenue
+    FORMAT("%'d", totrevenue) AS revenue
 FROM `project.dataset.table`
-ORDER BY total_revenue DESC 
+ORDER BY revenue DESC 
 LIMIT 10 # make sure you limit your query, because otherwise it just waste of many byte amounts of processing
+
+# standardSQL - Income
+SELECT
+    (totrevenue - totfuncexpns) AS income
+FROM `project.dataset.table` # `bigquery-public-data.irs_990.irs_990_2015`
+WHERE income > 0
+ORDER BY income DESC
+LIMIT 10
