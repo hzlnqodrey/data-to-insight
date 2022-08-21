c385d50abfa5f8d839461ba57cd7ce2063157343
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-21 17:14:26 +0700

    add name of the lab

--- 2 - Creating BigQuery - Visualizing Insights/Lab/3 - Lab 3/lab-work.sql
+++ 2 - Creating BigQuery - Visualizing Insights/Lab/3 - Lab 3/lab-work.sql
@@ -1,3 +1,4 @@
+-- Creating Date-Partitioned Tables in BigQuery v1.5
 -- # Creating tables with date partitions
 -- A partitioned table is a table that is divided into segments, called partitions, that make it easier to manage and query your data. By dividing a large table into smaller partitions, you can improve query performance, and control costs by reducing the number of bytes read by a query.
 
