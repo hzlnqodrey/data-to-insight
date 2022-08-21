3cbf3b82fd18bcf6b984f9c423842af5c1229799
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-21 17:41:27 +0700

    add noaa_gsod days with rain and create partition by getting from public dataset

--- 2 - Creating BigQuery - Visualizing Insights/Lab/3 - Lab 3/lab-work.sql
+++ 2 - Creating BigQuery - Visualizing Insights/Lab/3 - Lab 3/lab-work.sql
@@ -140,6 +140,7 @@ You should see This query will process 0 B when run.
    AND CAST(_TABLE_SUFFIX AS int64) >= 2018
  ORDER BY date DESC -- Where has it rained/snowed recently
  LIMIT 10
+ 
 -- Copied!
 -- Note that the table wildcard * used in the FROM clause to limit the amount of tables referred to in the TABLE_SUFFIX filter.
 
@@ -148,3 +149,77 @@ You should see This query will process 0 B when run.
 -- Click RUN.
 
 -- Confirm the date is properly formatted and the precipitation field is showing non-zero values.
+
+-- Your turn: Create a Partitioned Table
+-- Modify the previous query to create a table with the below specifications:
+
+-- Table name: ecommerce.days_with_rain
+-- Use the date field as your PARTITION BY
+-- For OPTIONS, specify partition_expiration_days = 60
+-- Add the table description = "weather stations with precipitation, partitioned by day"
+-- Your query should look like this:
+
+#standardSQL
+ CREATE OR REPLACE TABLE ecommerce.days_with_rain
+ PARTITION BY date
+ OPTIONS (
+   partition_expiration_days=60,
+   description="weather stations with precipitation, partitioned by day"
+ ) AS
+ SELECT
+   DATE(CAST(year AS INT64), CAST(mo AS INT64), CAST(da AS INT64)) AS date,
+   (SELECT ANY_VALUE(name) FROM `bigquery-public-data.noaa_gsod.stations` AS stations
+    WHERE stations.usaf = stn) AS station_name,  -- Stations may have multiple names
+   prcp
+ FROM `bigquery-public-data.noaa_gsod.gsod*` AS weather
+ WHERE prcp < 99.9  -- Filter unknown values
+   AND length(_TABLE_SUFFIX) = 4 AND CAST(_TABLE_SUFFIX AS int64) >= 2018
+   AND prcp > 0      -- Filter
+   AND CAST(_TABLE_SUFFIX AS int64) >= 2018
+
+-- Copied!
+-- Confirm data partition expiration is working
+-- To confirm you are only storing data from 60 days in the past up until today, run the DATE_DIFF query to get the age of your partitions, which are set to expire after 60 days.
+
+-- Below is a query which tracks the average rainfall for the NOAA weather station in Wakayama, Japan which has significant precipitation.
+
+-- Add this query and run it:
+
+#standardSQL
+# avg monthly precipitation
+SELECT
+  AVG(prcp) AS average,
+  station_name,
+  date,
+  CURRENT_DATE() AS today,
+  DATE_DIFF(CURRENT_DATE(), date, DAY) AS partition_age,
+  EXTRACT(MONTH FROM date) AS month
+FROM ecommerce.days_with_rain
+WHERE station_name = 'WAKAYAMA' #Japan
+GROUP BY station_name, date, today, month, partition_age
+ORDER BY date DESC; # most recent days first
+
+-- Copied!
+-- Confirm the oldest partition_age is at or below 60 days
+-- Update the ORDER BY clause to show the oldest partitions first. The date you see there Add this query and run it:
+
+#standardSQL
+# avg monthly precipitation
+SELECT
+  AVG(prcp) AS average,
+  station_name,
+  date,
+  CURRENT_DATE() AS today,
+  DATE_DIFF(CURRENT_DATE(), date, DAY) AS partition_age,
+  EXTRACT(MONTH FROM date) AS month
+FROM ecommerce.days_with_rain
+WHERE station_name = 'WAKAYAMA' #Japan
+GROUP BY station_name, date, today, month, partition_age
+ORDER BY partition_age DESC
+
+-- Copied!
+-- Note: Your results will vary if you re-run the query in the future, as the weather data, and your partitions, are continuously updated.
+-- Congratulations!
+-- You've successfully created and queried partitioned tables in BigQuery.
+
+-- End your lab
