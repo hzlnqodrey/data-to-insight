8932c36a4bfb8ea4b32350dea8a453f901c4802e
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-21 17:11:07 +0700

    explore the data in the non-partitioned table first.

--- 2 - Creating BigQuery - Visualizing Insights/Lab/3 - Lab 3/lab-work.sql
+++ 2 - Creating BigQuery - Visualizing Insights/Lab/3 - Lab 3/lab-work.sql
@@ -4,3 +4,21 @@
 -- Now you will create a new table and bind a date or timestamp column as a partition. Before we do that, let's explore the data in the non-partitioned table first.
 
 
+-- Query webpage analytics for a sample of visitors in 2017
+-- In the Query Editor, add the below query. Before running, note the total amount of data it will process as indicated next to the query validator icon: "This query will process 1.74 GB when run".
+
+#standardSQL
+SELECT DISTINCT
+  fullVisitorId,
+  date,
+  city,
+  pageTitle
+FROM `data-to-insights.ecommerce.all_sessions_raw`
+WHERE date = '20170708'
+LIMIT 5
+-- Copied!
+-- Click RUN.
+
+-- The query returns 5 results.
+
+-- Query webpage analytics for a sample of visitor
