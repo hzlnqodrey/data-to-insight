3490e0ae6075f834d1c35ea1b3f0601f83e6414f
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-09-12 02:16:27 +0700

    Query relational data

--- 3 - Achieving Advanced Insights with BigQuery/Lab/3 - Lab 3/Optimizing BigQuery for Cost and Performance v1.5/lab_work.sql
+++ 3 - Achieving Advanced Insights with BigQuery/Lab/3 - Lab 3/Optimizing BigQuery for Cost and Performance v1.5/lab_work.sql
@@ -1,7 +1,7 @@
 Load and query relational data
 In this section, you measure query performance for relational data in BigQuery.
 
-BigQuery supports large JOINs, and JOIN performance is good. However, BigQuery is a columnar datastore, and maximum performance is achieved on denormalized datasets. Because BigQuery storage is inexpensive and scalable, it's a good practice to denormalize and pre-JOIN datasets into homogeneous tables. In other words, you exchange compute resources for storage resources (the latter being more performant and cost-effective).
+BigQuery supports large JOINs, and JOIN performance is good. However, BigQuery is a columnar datastore, and maximum performance is achieved on denormalized datasets. Because BigQuery storage is inexpensive and scalable, it is a good practice to denormalize and pre-JOIN datasets into homogeneous tables. In other words, you exchange compute resources for storage resources (the latter being more performant and cost-effective).
 
 In this section, you:
 
@@ -18,7 +18,7 @@ category	The grouping to which the item belongs.
 store	The store that sold th item.
 county	The county where the item was sold.
 convenience_store	The list of stores that are considered convenience stores.
-Here's a diagram of the relational schema:
+Heres a diagram of the relational schema:
 
 1.png
 
@@ -35,7 +35,7 @@ Enter the path to the Google Cloud Storage bucket name:
 data-insights-course/labs/optimizing-for-performance/sales.csv
 Copied!
 For File format, choose CSV.
-Note: When you've created a table previously, the Select Previous Job option allows you to quickly use your settings to create similar tables.
+Note: When youve created a table previously, the Select Previous Job option allows you to quickly use your settings to create similar tables.
 
 In the Destination section:
 
@@ -97,3 +97,99 @@ Accept the remaining default values and click Create Table.
 4.png
 
 BigQuery creates a load job to create the table and upload data into the table (this may take a few seconds). Click Personal History to track job progress.
+
+Create the remaining tables
+Create the remaining tables in the relational schema using Cloud Shell command line.
+
+Create the category table:
+
+bq load --source_format=CSV --skip_leading_rows=1 --allow_quoted_newlines liquor_sales.category gs://data-insights-course/labs/optimizing-for-performance/category.csv category:STRING,category_name:STRING
+Copied!
+Create the convenience_store table:
+
+bq load --source_format=CSV --skip_leading_rows=1 --allow_quoted_newlines liquor_sales.convenience_store gs://data-insights-course/labs/optimizing-for-performance/convenience_store.csv store:STRING
+Copied!
+Create the county table:
+
+bq load --source_format=CSV --skip_leading_rows=1 --allow_quoted_newlines liquor_sales.county gs://data-insights-course/labs/optimizing-for-performance/county.csv county_number:STRING,county:STRING
+Copied!
+Create the item table:
+
+bq load --source_format=CSV --skip_leading_rows=1 --allow_quoted_newlines liquor_sales.item gs://data-insights-course/labs/optimizing-for-performance/item.csv item:STRING,description:string,pack:INTEGER,liter_size:INTEGER
+Copied!
+Create the store table:
+
+bq load --source_format=CSV --skip_leading_rows=1 --allow_quoted_newlines liquor_sales.store gs://data-insights-course/labs/optimizing-for-performance/store.csv store:STRING,name:STRING,address:STRING,city:STRING,zipcode:STRING,store_location:STRING,county_number:STRING
+Copied!
+Create the vendor table:
+
+bq load --source_format=CSV --skip_leading_rows=1 --allow_quoted_newlines liquor_sales.vendor gs://data-insights-course/labs/optimizing-for-performance/vendor.csv vendor_no:STRING,vendor:STRING
+Copied!
+Go back to the BigQuery web UI. Confirm you see the new tables loaded into your liquor_sales dataset. Refresh the browser if needed.
+
+f82c581950144db1.png
+
+Query relational data
+You use the Query editor to query your data.
+
+Under the Query editor code box, click More > Query Settings.
+
+In the Resource management, Cache preference section, uncheck Use Cached Results and click Save. If you have to run the query more than once, you don't want to use cached results.
+
+In the Query editor window, enter the following query against the relational tables and click Run.
+
+#standardSQL
+SELECT
+  gstore.county AS county,
+  ROUND(cstore_total/gstore_total * 100,1) AS cstore_percentage
+FROM (
+  SELECT
+    cy.county AS county,
+    SUM(total) AS gstore_total
+  FROM
+    `liquor_sales.sales` AS s
+  JOIN
+    `liquor_sales.store` AS st
+  ON
+    s.store = st.store
+  JOIN
+    `liquor_sales.county` AS cy
+  ON
+    st.county_number = cy.county_number
+  LEFT OUTER JOIN
+    `liquor_sales.convenience_store` AS c
+  ON
+    s.store = c.store
+  WHERE
+    c.store IS NULL
+  GROUP BY
+    county) AS gstore
+JOIN (
+  SELECT
+    cy.county AS county,
+    SUM(total) AS cstore_total
+  FROM
+    `liquor_sales.sales` AS s
+  JOIN
+    `liquor_sales.store` AS st
+  ON
+    s.store = st.store
+  JOIN
+    `liquor_sales.county` AS cy
+  ON
+    st.county_number = cy.county_number
+  LEFT OUTER JOIN
+    `liquor_sales.convenience_store` AS c
+  ON
+    s.store = c.store
+  WHERE
+    c.store IS NOT NULL
+  GROUP BY
+    county) AS hstore
+ON
+  gstore.county = hstore.county
+Copied!
+At the bottom, in the Query results section, click on the Results tab, note the Query complete time. An example is shown below (your execution time may vary).
+Query_results6.png
+
+This will be compared to the time to query a flattened dataset in later sections.
